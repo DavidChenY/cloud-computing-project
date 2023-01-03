ba63d968116a87332ec301157e409ccb63c67204
DESKTOP-B1HIPAF\Jakob <jakob.peham.jp@gmail.com>
2023-01-03 12:57:57 +0100

    Add calculator logic

--- calculator/src/main/java/at/jku/calculator/controller/CalculateController.java
+++ calculator/src/main/java/at/jku/calculator/controller/CalculateController.java
@@ -1,10 +1,11 @@
 package at.jku.calculator.controller;
 
+import at.jku.calculator.entity.Request;
 import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Random;
@@ -22,5 +23,39 @@ public class CalculateController {
     @GetMapping("random/{count}")
     public List<Integer> getRandomNumbers(@PathParam("count") int count) {
         return new Random().ints(count).boxed().collect(Collectors.toList());
+    }
+
+    @PostMapping("calculate")
+    public ResponseEntity<Object> calculate(@RequestBody Request request) {
+        float result = mysteriousFunction(request.getFirstNumber(), request.getSecondNumber(), request.getOperation());
+        JSONObject entity = new JSONObject();
+        entity.put("result", result);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    private float mysteriousFunction(int firstNumber, int secondNumber, String operation) {
+        float result;
+        if(firstNumber==0 || firstNumber==1)
+            return 1f;
+
+        if(secondNumber==0 || secondNumber==1)
+            return 1f;
+
+        switch(operation){
+            case "+":
+                result = mysteriousFunction(firstNumber-1, secondNumber-1, operation) * firstNumber + mysteriousFunction(firstNumber-1, secondNumber-1, operation) * secondNumber;
+                break;
+            case "-":
+                result = mysteriousFunction(firstNumber-1, secondNumber-1, operation) * firstNumber - mysteriousFunction(firstNumber-1, secondNumber-1, operation) * secondNumber;
+                break;
+            case "*":
+                result = mysteriousFunction(firstNumber-1, secondNumber-1, operation) * firstNumber * mysteriousFunction(firstNumber-1, secondNumber-1, operation) * secondNumber;
+                break;
+            case "/":
+                result = mysteriousFunction(firstNumber-1, secondNumber-1, operation) * firstNumber / mysteriousFunction(firstNumber-1, secondNumber-1, operation) * secondNumber;
+                break;
+            default: return -1;
+        }
+        return result;
     }
 }
