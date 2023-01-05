8ab7b11645ec845d45ec216136fa84d58066ddd9
David Chen <dachyi1998@gmail.com>
2023-01-05 21:21:57 +0100

    Add calculation

---
+++ calculator/setup.md
@@ -0,0 +1,14 @@
+# Setup for calculator
+
+## Instructions
+
+1. Build a Docker image based on the Dockerfile:
+    ```console
+    docker image build -f Dockerfile -t davidchen98/calculator ./
+    ```
+   
+2. List all images that are stored in your local registry:
+
+    ```console
+    docker images
+    ```
---
+++ calculator/src/main/java/at/jku/calculator/entity/Result.java
@@ -0,0 +1,21 @@
+package at.jku.calculator.entity;
+
+
+public class Result {
+    private float result;
+
+    public Result() {
+    }
+
+    public Result(float result) {
+        this.result = result;
+    }
+
+    public float getResult() {
+        return result;
+    }
+
+    public void setResult(float result) {
+        this.result = result;
+    }
+}
