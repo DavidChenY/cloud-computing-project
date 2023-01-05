fcfa5eaf93db8e47c25471a97716d77c6e4d19a2
DESKTOP-B1HIPAF\Jakob <jakob.peham.jp@gmail.com>
2023-01-05 22:05:58 +0100

    Refactor storage

--- storage/src/main/java/at/jku/storage/controller/CalculationController.java
+++ storage/src/main/java/at/jku/storage/controller/CalculationController.java
@@ -29,38 +29,27 @@ public class CalculationController {
 
     @PostMapping("/calculate")
     @ResponseBody
-    public ResponseEntity<Object> calculate(@RequestBody Request request) {
+    public Result calculate(@RequestBody Request request) {
         Calculation calculation = calculationRepository.findByFirstNumberAndSecondNumberAndOperation(request.getFirstNumber(), request.getSecondNumber(), request.getOperation());
 
         if(calculation != null) {
-            return returnJSON("result", calculation.getResult());
+            return new Result(calculation.getResult());
         } else {
             float result = callCalculator(request.getFirstNumber(), request.getSecondNumber(), request.getOperation());
             calculationRepository.save(new Calculation(request.getFirstNumber(), request.getSecondNumber(), request.getOperation(), result));
-            return returnJSON("result", result);
+            return new Result(result);
         }
     }
 
     private Float callCalculator(int first, int second, String operation) {
         String uri = "http://localhost:8081/calculate"; // TODO: change to real URL
-        String body = getBody(first, second, operation);
+        Request request = new Request(first, second, operation);
         WebClient.ResponseSpec clientResponse = WebClient.builder().build()
-                .post().uri(uri).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(body)).retrieve();
+                .post().uri(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(request)).retrieve();
 
         return Objects.requireNonNull(clientResponse.bodyToMono(Result.class).block()).getResult();
-    }
-
-    private String getBody(int first, int second, String operation) {
-        return "{\n" +
-                "\"firstNumber\":"+first+",\n"+
-                "\"secondNumber\":"+second+",\n"+
-                "\"operation\":"+"\""+operation+"\"\n"+
-                "}";
-    }
-
-    private ResponseEntity<Object> returnJSON(String key, float value) {
-        JSONObject entity = new JSONObject();
-        entity.put(key, value);
-        return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 }
